6967a9d06fdfa41cc651df34e3e6ad85ab625a97
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-01 19:30:15 +0100

    'Iteration 1 | Display 5 Contacts'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import contacts from "./contacts.json";
-import './App.css';
+import React, { useState } from "react";
+import contactsData from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+
   return (
-    <div className="App">
-        <h1>Lab Contacts</h1>
-        
+    <div className="contacts-card">
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr className="card-title">
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody className="body-card">
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  className="card-img"
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                />
+              </td>
+              <td className="card-name">{contact.name}</td>
+              <td className="card-popularity">
+                {contact.popularity.toFixed(2)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
